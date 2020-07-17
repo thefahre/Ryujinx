3aa0328ee326266569cb88c39cd3616315b5d5d3
riperiperi <rhy3756547@hotmail.com>
2020-07-18 00:43:05 +0100

    Dispose -> Release

--- Ryujinx.Graphics.OpenGL/Image/TextureView.cs
+++ Ryujinx.Graphics.OpenGL/Image/TextureView.cs
@@ -412,10 +412,8 @@ namespace Ryujinx.Graphics.OpenGL.Image
             throw new NotSupportedException();
         }
 
-        public void Dispose()
+        public void Release()
         {
-            // TODO: this is really "release".
-
             bool isDefault = _parent._defaultView == this;
 
             if (_incompatibleFormatView != null)
@@ -445,11 +443,11 @@ namespace Ryujinx.Graphics.OpenGL.Image
             }
         }
 
-        public void TrueDispose()
+        public void Dispose()
         {
             _parent.DeleteDefault();
 
-            Dispose();
+            Release();
         }
     }
 }
